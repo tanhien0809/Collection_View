13b8aa31161fec27d319649e776828ac3359bea1
tanhien0809 <hien@Hiens-Mac.local>
2017-04-28 12:59:09 +0700

    Up

---
+++ Collection_View/MyCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MyCollectionViewCell.swift
+//  Collection_View
+//
+//  Created by Hien on 4/28/17.
+//  Copyright © 2017 Hien. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak internal var myImageView: UIImageView!
+}
--- Collection_View/ViewController.swift
+++ Collection_View/ViewController.swift
@@ -8,16 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    @IBOutlet weak var MyCollectionView: UICollectionView!
+    var images = ["One", "Two", "Three", "Four", "Five"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.MyCollectionView.delegate = self
+        self.MyCollectionView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Collection_Cell", for: indexPath) as! MyCollectionViewCell
+        cell.myImageView.image = UIImage(named: images[indexPath.row])
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        print("Selected row is", indexPath.row)
     }
 
 
